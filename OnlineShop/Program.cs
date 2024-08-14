e8260c1786dc820fbc22d99d1f653990991f86b1
main <cornellcornelius@icloud.com>
2024-08-14 12:06:21 -0500

    update

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -9,14 +9,16 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 // Cache control is on with this statement
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 // Cache control is off with this statement
+/*
 builder.Services.AddScoped(sp => new HttpClient { 
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
     DefaultRequestHeaders = {
         CacheControl = new CacheControlHeaderValue { NoCache = true }
     }
 });
+*/
 builder.Services.AddScoped<CartState>();
 builder.Services.AddSingleton<INotificationService, NotificationService>();
 
